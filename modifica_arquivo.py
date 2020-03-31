3aff8a4e9f70421c596852b63801baf82ad1b97b
WhySoFast <matheus.carvalho@outlook.com>
2020-03-31 17:47:41 -0300

    Teste de modificação

---
+++ modifica_arquivo.py
@@ -0,0 +1,21 @@
+import shutil
+
+original = r'MAT.txt'
+file_path = r'SecMAT.txt' 
+shutil.copyfile(original, file_path)
+
+match_string = "BANANA"
+insert_string = "//"
+with open(file_path, 'r+') as fd:
+    contents = fd.readlines()
+    if match_string in contents[-1]:  # Handle last line to prevent IndexError
+        contents.append(insert_string)
+    else:
+        for index, line in enumerate(contents):
+            print(index)
+            if match_string in line and insert_string not in contents[index + 1]:
+                contents.insert(index , insert_string)
+                break
+    fd.seek(0)
+    fd.writelines(contents)
+    fd.close()
