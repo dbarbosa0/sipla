c849f2fa0aea4cbbaeb285494bfc37a534fd5b2e
nyegirton <nyegirton@gmail.com>
2020-09-24 15:20:00 -0300

    Nyegirton

--- opendss/VoltVarWatt/class_InsertMassivePV.py
+++ opendss/VoltVarWatt/class_InsertMassivePV.py
@@ -21,11 +21,19 @@ class C_Insert_MassivePV(C_OpenDSSDirect_Conn):
          for numero in range(0, len(self.AllBusNames)):
             name = self.AllBusNames[numero]
             opendssdirect.run_command(
-                "New PVSystem.{bus_name} Bus1={bus_name} phases=1 kVA=600 irrad=0.98 Pmpp=500 temperature=25 PF=1 %cutin=0.1 %cutout=0.1".format(
+                "New Transformer.{bus_name} phases=3 xhl=5.75 wdg=1 Bus'={bus_name} kv={tensao} conn=wye wdg=2 bus={ordem} kv=220 conn=wye".format(
+                    tensao=opendssdirect.Bus.kVBase(),
                     bus_name=name,
+                    ordem=numero
+                )
+            )
+            opendssdirect.run_command(
+                "New PVSystem.{bus_n} Bus1={bus_n} phases=1 kVA=600 irrad=0.98 Pmpp=500 temperature=25 PF=1 %cutin=0.1 %cutout=0.1".format(
+                    bus_n=numero
                 )
             )
          print(opendssdirect.PVsystems.AllNames())
+         print(opendssdirect.Transformers.AllNames())
          self.OpenDSS = opendss.class_opendss.C_OpenDSS()
 
     def Solve(self):
